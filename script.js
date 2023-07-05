c6d988959f72ed029658d34765b98280b995cefa
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-07-05 15:02:45 -0700

    grabbing dom elements and declaring variables for timer, time. question Index, and the scroe

---
+++ script.js
@@ -0,0 +1,16 @@
+var startButton = document.getElementById('start');
+var timerDisplay = document.getElementById('time');
+var questionDisplay = document.getElementById('question');
+var optionsDisplay = document.getElementById('options');
+var endScreen = document.getElementById('end-screen');
+var scoreDisplay = document.getElementById('score');
+var initialsInput = document.getElementById('initials');
+var saveButton = document.getElementById('save');
+
+var timer;
+var time = 60;
+var questionIndex = 0;
+var score = 0;
+
+
+
